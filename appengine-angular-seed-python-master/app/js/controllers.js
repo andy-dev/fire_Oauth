7617de294c6e24dcaa8216a73fff1930423ef212
andy-dev <andres.macedo@gmail.com>
2015-05-14 16:25:39 -0700

    add basic setup, landingpage and bootstrap

--- appengine-angular-seed-python-master/app/js/controllers.js
+++ appengine-angular-seed-python-master/app/js/controllers.js
@@ -2,10 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController', [function(){
 
   }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
